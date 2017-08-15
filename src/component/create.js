388a66faf5796d09f5b698a5e28307960fe15c2e
hengkx <ycxzhkx@gmail.com>
2017-08-15 14:34:27 +0800

    add function component

--- src/component/create.js
+++ src/component/create.js
@@ -20,95 +20,13 @@ export default function createComponent(config, opts = { type: 0 }) {
                 { name: name })
         );
 
-        const lifecycle = {};
         const module = {};
-        const firstLoad = [];
-
-        _.forEach(opts.lifecycle, (item) => lifecycle[item] = true);
         _.forEach(opts.module, (item) => {
             module[item] = camelCaseName;
             if (item !== 'style' && item !== 'doc') {
                 mkdirsSync(join(componentPath, config.directory[item] || item));
             }
         });
-
-        if (opts.type === 1) {
-            const reduxPath = join(config.dir, config.directory.source, config.directory.redux);
-            mkdirsSync(reduxPath);
-            const actionGroups = {};
-            const exportActions = [];
-            const mapStateToProps = [];
-
-            const apiPath = join(config.dir, config.directory.source, config.directory.config, 'api.js');
-            let apiContent = readFileSync(apiPath, 'utf8');
-            apiContent = apiContent.replace('export default', '').replace(';', '');
-            apiContent = JSON.parse(apiContent);
-            apiContent[opts.sagas[0].requestVarName] = opts.sagas[0].url;
-            writeFileSync(apiPath, `export default ${JSON.stringify(apiContent, null, 2)};`);
-
-            opts.sagas.forEach(item => {
-                const actionType = camelCase(item.actionName);
-                writeFileSync(join(reduxPath, `${camelCaseName}.js`),
-                    render('redux/saga.mustache',
-                        {
-                            ...item,
-                            method: item.method.toLowerCase(),
-                            actionName: item.actionName.toUpperCase(),
-                            prefix: camelCaseName.toUpperCase(),
-                            actionType
-                        })
-                );
-                actionGroups[camelCaseName] = [];
-                exportActions.push(actionType);
-                actionGroups[camelCaseName].push(actionType);
-                mapStateToProps.push(`${actionType}Result: state.${name}.${actionType}Result`);
-            });
-
-            const containerPath = join(config.dir, config.directory.source, config.directory.container);
-            mkdirsSync(containerPath);
-            const { actions = [], first = [] } = opts.import;
-            first.forEach(item => {
-                const [name, actionName] = item.split(' ');
-                const actionType = _.camelCase(actionName);
-                firstLoad.push(`this.props.${actionType}()`);
-            });
-            let importContent = '';
-            actions.forEach(item => {
-                const [name, actionName] = item.split(' ');
-                if (!actionGroups[name]) {
-                    actionGroups[name] = [];
-                }
-                const actionType = _.camelCase(actionName);
-                exportActions.push(actionType);
-                actionGroups[name].push(actionType);
-                mapStateToProps.push(`${actionType}Result: state.${_.camelCase(name)}.${actionType}Result`);
-            });
-            Object.keys(actionGroups).forEach(key => {
-                importContent += `import { ${actionGroups[key].join(', ')} } from '../${config.directory.redux}/${key}';\n`;
-            });
-
-            writeFileSync(join(containerPath, `${name}.js`),
-                render('container/container.mustache',
-                    {
-                        ...opts,
-                        componentDir: config.directory.component,
-                        importContent,
-                        mapStateToProps: mapStateToProps.join(',\n'),
-                        exportActions: exportActions.join(', '),
-                    })
-            );
-
-            reduxReset(reduxPath);
-        }
-
-        writeFileSync(join(componentPath, `${name}.js`),
-            render('component/component.mustache', {
-                ...opts,
-                lifecycle,
-                firstLoad: firstLoad.join(';\n'),
-                module
-            })
-        );
 
         if (module.style) {
             const stylePath = join(componentPath, config.directory.style);
@@ -120,6 +38,99 @@ export default function createComponent(config, opts = { type: 0 }) {
             mkdirsSync(docPath);
             writeFileSync(join(docPath, 'index.md'), `${name} Doc`);
         }
+
+        if (opts.format !== 'function') {
+            const lifecycle = {};
+            const firstLoad = [];
+
+            _.forEach(opts.lifecycle, (item) => lifecycle[item] = true);
+
+            if (opts.type === 1) {
+                const reduxPath = join(config.dir, config.directory.source, config.directory.redux);
+                mkdirsSync(reduxPath);
+                const actionGroups = {};
+                const exportActions = [];
+                const mapStateToProps = [];
+
+                const apiPath = join(config.dir, config.directory.source, config.directory.config, 'api.js');
+                let apiContent = readFileSync(apiPath, 'utf8');
+                apiContent = apiContent.replace('export default', '').replace(';', '');
+                apiContent = JSON.parse(apiContent);
+                apiContent[opts.sagas[0].requestVarName] = opts.sagas[0].url;
+                writeFileSync(apiPath, `export default ${JSON.stringify(apiContent, null, 2)};`);
+
+                opts.sagas.forEach(item => {
+                    const actionType = camelCase(item.actionName);
+                    writeFileSync(join(reduxPath, `${camelCaseName}.js`),
+                        render('redux/saga.mustache',
+                            {
+                                ...item,
+                                method: item.method.toLowerCase(),
+                                actionName: item.actionName.toUpperCase(),
+                                prefix: camelCaseName.toUpperCase(),
+                                actionType
+                            })
+                    );
+                    actionGroups[camelCaseName] = [];
+                    exportActions.push(actionType);
+                    actionGroups[camelCaseName].push(actionType);
+                    mapStateToProps.push(`${actionType}Result: state.${name}.${actionType}Result`);
+                });
+
+                const containerPath = join(config.dir, config.directory.source, config.directory.container);
+                mkdirsSync(containerPath);
+                const { actions = [], first = [] } = opts.import;
+                first.forEach(item => {
+                    const [name, actionName] = item.split(' ');
+                    const actionType = _.camelCase(actionName);
+                    firstLoad.push(`this.props.${actionType}()`);
+                });
+                let importContent = '';
+                actions.forEach(item => {
+                    const [name, actionName] = item.split(' ');
+                    if (!actionGroups[name]) {
+                        actionGroups[name] = [];
+                    }
+                    const actionType = _.camelCase(actionName);
+                    exportActions.push(actionType);
+                    actionGroups[name].push(actionType);
+                    mapStateToProps.push(`${actionType}Result: state.${_.camelCase(name)}.${actionType}Result`);
+                });
+                Object.keys(actionGroups).forEach(key => {
+                    importContent += `import { ${actionGroups[key].join(', ')} } from '../${config.directory.redux}/${key}';\n`;
+                });
+
+                writeFileSync(join(containerPath, `${name}.js`),
+                    render('container/container.mustache',
+                        {
+                            ...opts,
+                            componentDir: config.directory.component,
+                            importContent,
+                            mapStateToProps: mapStateToProps.join(',\n'),
+                            exportActions: exportActions.join(', '),
+                        })
+                );
+
+                reduxReset(reduxPath);
+            }
+
+            writeFileSync(join(componentPath, `${name}.js`),
+                render('component/component.mustache', {
+                    ...opts,
+                    lifecycle,
+                    firstLoad: firstLoad.join(';\n'),
+                    module
+                })
+            );
+        } else {
+            writeFileSync(join(componentPath, `${name}.js`),
+                render('component/function.mustache', {
+                    ...opts,
+                    module
+                })
+            );
+        }
+
         writeFileSync(join(vdConfigPath, `${camelCaseName}.json`), JSON.stringify(opts, null, 2));
     }
 }
