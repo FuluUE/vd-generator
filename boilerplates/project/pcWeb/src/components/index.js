bebb25eda17b7640c75283cee5c8d71e395c87c7
yincw <120758164@qq.com>
2017-09-04 12:02:08 +0800

    index.js

---
+++ boilerplates/project/pcWeb/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Bundle } from './Bundle';
+export { default as Icon } from './Icon';
+export { default as Main } from './Main';
+export { default as PageNotFound } from './PageNotFound';
+export { default as PageServerError } from './PageServerError';
