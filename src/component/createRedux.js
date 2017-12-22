007f7bd13acc1d9683790d12c1603c886d94fcf0
hengkx <ycxzhkx@gmail.com>
2017-12-22 14:44:30 +0800

    add common to redux component

--- src/component/createRedux.js
+++ src/component/createRedux.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import { join } from 'path';
-import { existsSync, mkdirsSync, writeFileSync, readFileSync } from 'fs-extra';
+import { existsSync, mkdirsSync, writeFileSync, readFileSync, readJSONSync } from 'fs-extra';
 import { camelCase } from '../utils';
 import reduxReset from './reduxReset';
 import render from '../utils/mustache';
@@ -11,58 +11,63 @@ export default (config, opts) => {
     const camelCaseName = camelCase(name);
     opts.camelCaseName = camelCaseName;
     const componentPath = join(config.dir, config.directory.source, config.directory.component, name);
-    mkdirsSync(componentPath);
     const vdConfigPath = join(config.dir, '.vd', 'components');
     mkdirsSync(vdConfigPath);
 
-    writeFileSync(join(componentPath, 'index.js'),
-        render('component/index.mustache',
-            { name: name })
-    );
+    if (!existsSync(componentPath)) {
+        mkdirsSync(componentPath);
+        writeFileSync(join(componentPath, 'index.js'),
+            render('component/index.mustache',
+                { name: name })
+        );
 
-    const module = {};
-    _.forEach(opts.module, (item) => {
-        module[item] = camelCaseName;
-        if (item !== 'style' && item !== 'doc' && item !== '__tests__') {
-            mkdirsSync(join(componentPath, config.directory[item] || item));
+        const module = {};
+        _.forEach(opts.module, (item) => {
+            module[item] = camelCaseName;
+            if (item !== 'style' && item !== 'doc' && item !== '__tests__') {
+                mkdirsSync(join(componentPath, config.directory[item] || item));
+            }
+        });
+
+        if (module.__tests__) {
+            const testPath = join(componentPath, '__tests__');
+            mkdirsSync(testPath);
+            writeFileSync(join(testPath, `${camelCaseName}.js`),
+                render('component/test.mustache', {
+                    name
+                }));
         }
-    });
 
-    if (module.__tests__) {
-        const testPath = join(componentPath, '__tests__');
-        mkdirsSync(testPath);
-        writeFileSync(join(testPath, `${camelCaseName}.js`),
-            render('component/test.mustache', {
-                name
-            }));
-    }
+        if (module.style) {
+            const stylePath = join(componentPath, config.directory.style);
+            mkdirsSync(stylePath);
+            writeFileSync(join(stylePath, `${camelCaseName}.less`), '');
+        }
+        if (module.doc) {
+            const docPath = join(componentPath, config.directory.document);
+            mkdirsSync(docPath);
+            writeFileSync(join(docPath, 'index.md'), `${name} Doc`);
+        }
 
-    if (module.style) {
-        const stylePath = join(componentPath, config.directory.style);
-        mkdirsSync(stylePath);
-        writeFileSync(join(stylePath, `${camelCaseName}.less`), '');
-    }
-    if (module.doc) {
-        const docPath = join(componentPath, config.directory.document);
-        mkdirsSync(docPath);
-        writeFileSync(join(docPath, 'index.md'), `${name} Doc`);
-    }
+        const lifecycle = {};
+        const firstLoad = [];
 
-    const lifecycle = {};
-    const firstLoad = [];
+        _.forEach(opts.lifecycle, (item) => lifecycle[item] = true);
 
-    _.forEach(opts.lifecycle, (item) => lifecycle[item] = true);
-
-    writeFileSync(join(componentPath, `${name}.js`),
-        render('component/component.mustache', {
+        writeFileSync(join(componentPath, `${name}.js`),
+            render('component/component.mustache', {
+                ...opts,
+                lifecycle,
+                firstLoad: firstLoad.join(';\n'),
+                module
+            })
+        );
+    } else {
+        opts = {
+            ...readJSONSync(join(vdConfigPath, `${camelCaseName}.json`)),
             ...opts,
-            lifecycle,
-            firstLoad: firstLoad.join(';\n'),
-            module
-        })
-    );
-
-
+        }
+    }
     if (opts.saga) {
         const apiPath = join(config.dir, config.directory.source, config.directory.config, 'api.js');
         let apiContent = readFileSync(apiPath, 'utf8');
