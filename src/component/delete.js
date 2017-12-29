0faa4cb11d69ea5fc5f8da170efa64bdd516a25a
hengkx <ycxzhkx@gmail.com>
2017-12-29 09:53:39 +0800

    fix del componet

--- src/component/delete.js
+++ src/component/delete.js
@@ -5,22 +5,30 @@ import resetIndex from './resetIndex';
 
 export default (project, component) => {
     const dev = project.directory.development;
-    const { name, type } = component;
-    removeSync(join(project.dir, '.vd', 'components', `${name}.json`));
-    removeSync(join(project.dir, dev.envName, dev.component, name));
+    const { name, type, group } = component;
+
+    let filename = `${name}.json`;
+    if (group) {
+        filename = `${group.replace(/>/g, '-')}-${name}.json`;
+    }
+
+    removeSync(join(project.dir, '.vd', 'components', filename));
+
+    let baseComponentPath = join(project.dir, 'src', 'components');
+
+    if (group) {
+        let paths = group.split('>').map(e => e.trim());
+        paths.forEach(item => baseComponentPath = join(baseComponentPath, item));
+    }
+
+    removeSync(join(baseComponentPath, name));
+
     if (type === 1) {
-        removeSync(join(project.dir, dev.envName, dev.container, `${name}.js`));
-        // removeSync(join(project.dir, dev.envName, dev.redux, `${name}.js`));
-        removeSync(join(project.dir, dev.envName, 'models', `${name}.js`));
-        removeSync(join(project.dir, dev.envName, 'routes', `${name}.js`));
-        removeSync(join(project.dir, dev.envName, 'services', `${name}.js`));
-        // reduxReset(join(project.dir, dev.envName, dev.redux));
+        removeSync(join(project.dir, 'src', 'models', `${name}.js`));
+        removeSync(join(project.dir, 'src', 'routes', `${name}.js`));
+        removeSync(join(project.dir, 'src', 'services', `${name}.js`));
     }
     resetIndex({
-        ...project,
-        directory: {
-            source: dev.envName,
-            component: dev.component,
-        }
+        ...project
     });
 }
