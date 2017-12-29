31a24c106fa5720d35e6761c3cd859235c769522
hengkx <ycxzhkx@gmail.com>
2017-12-29 10:33:13 +0800

    fix index

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-exports.createProject = require('./lib/project/create');
-exports.getAllActions = require('./lib/project/getAllActions');
-exports.webpack = require('./lib/project/webpack');
 exports.createComponent = require('./lib/component/create');
 exports.createReduxComponent = require('./lib/component/createRedux');
 exports.deleteComponent = require('./lib/component/delete');
