6b35f05990cb1c67372262ac089a5c7598af855d
hengkx <ycxzhkx@gmail.com>
2017-12-29 14:58:00 +0800

    extract paths

--- src/component/delete.js
+++ src/component/delete.js
@@ -4,19 +4,14 @@ import resetIndex from './resetIndex';
 import { getPaths } from '../utils';
 
 export default (project, component) => {
-    const dev = project.directory.development;
     const { name, type, group } = component;
 
-    let filename = `${name}.json`;
-    if (group) {
-        filename = `${group.replace(/>/g, '-')}-${name}.json`;
-    }
     const paths = getPaths({ dir: project.dir, group, name });
 
     removeSync(paths.vdConfig);
     removeSync(paths.component);
 
-    if (type === 1) {
+    if (type === 1 || type === '1') {
         removeSync(join(paths.model, `${name}.js`));
         removeSync(join(paths.route, `${name}.js`));
         removeSync(join(paths.service, `${name}.js`));
